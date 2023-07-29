f289dcaa9545b03915f492fc9b1883fa8b614d87
Camille Sylla <camillesylla.web@gmail.com>
2023-07-29 02:23:33 +0200

    refactor(ctaegory-tree): sql query and build tree refactor

--- src/categories-tree/categories-tree.controller.ts
+++ src/categories-tree/categories-tree.controller.ts
@@ -1,23 +1,33 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Req, Controller, Post, Logger } from '@nestjs/common';
 import { CategoriesTreeService } from './categories-tree.service';
 
 @Controller('categories-tree')
 export class CategoriesTreeController {
+  private readonly logger = new Logger(CategoriesTreeController.name);
   constructor(private readonly categoriesTreeService: CategoriesTreeService) {}
 
   @Post('/webhook/build')
   async buildTree() {
-
-    const categories = await this.categoriesTreeService.getCategories();
-    const tree = this.categoriesTreeService.buildCategoryTree(categories);
-    const exist = (await this.categoriesTreeService.find()).filter(categoryTree => {
-        return categoryTree.id === 1;
-    });
-    if (!exist.length) {
+    try {
+      const categories = await this.categoriesTreeService.getCategories();      
+      const tree = this.categoriesTreeService.buildCategoryTree(categories);      
+      const exist = (await this.categoriesTreeService.find()).filter(
+        (categoryTree) => {
+          return categoryTree.id === 1;
+        },
+      );
+      if (!exist.length) {
         await this.categoriesTreeService.create(JSON.stringify(tree));
-    } else {
-        await this.categoriesTreeService.update(exist[0].id, JSON.stringify(tree));
+      } else {
+        await this.categoriesTreeService.update(
+          exist[0].id,
+          JSON.stringify(tree),
+        );
+      }
+      return tree;
+    } catch (error) {
+      this.logger.error(error);
+      throw error;
     }
-    return tree;
   }
 }
--- src/categories-tree/categories-tree.service.ts
+++ src/categories-tree/categories-tree.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { EntityManager, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CategoriesTreeEntity } from 'src/categories-tree/entity/categories-tree.entity';
 
 @Injectable()
 export class CategoriesTreeService {
+  private readonly logger = new Logger(CategoriesTreeService.name);
   constructor(
     @InjectRepository(CategoriesTreeEntity)
     private readonly categoriesTreeRepository: Repository<CategoriesTreeEntity>,
@@ -12,36 +13,60 @@ export class CategoriesTreeService {
   ) {}
 
   async getCategories() {
-    return this.entityManager.query('SELECT * FROM categories');
+    this.logger.log('Extracting categories');
+    return await this.entityManager.query(`SELECT
+    c.*,
+    json_build_object('id', pc.id, 'name', pc.name, 'slug', pc.slug) AS Parent
+FROM
+    categories AS c
+LEFT JOIN
+    categories_parent_links AS p ON c.id = p.category_id
+LEFT JOIN
+    categories AS pc ON p.inv_category_id = pc.id;`);
   }
 
   async find() {
-    return this.categoriesTreeRepository.find()
+    this.logger.log('Finding categories tree');
+    return this.categoriesTreeRepository.find();
   }
 
-  async create(tree: string){
+  async create(tree: string) {
+    this.logger.log('Creating category tree');
     return this.categoriesTreeRepository.save({ json: tree });
   }
 
-  async update(id: number, tree: string){
+  async update(id: number, tree: string) {
+    this.logger.log('Updating category tree');
     return this.categoriesTreeRepository.update(id, { json: tree });
   }
 
-buildCategoryTree(categories, parentId = null) {
-    const tree = [];
-  
-    for (const category of categories) {
-      if (
-        (parentId === null && !category.Parent) ||
-        (category.Parent && category.Parent.id === parentId)
-      ) {
-        const children = this.buildCategoryTree(categories, category.id);
-        const categoryWithChildren = { ...category, Childs: children };
-  
-        tree.push(categoryWithChildren);
+  buildCategoryTree(categories) {
+    const categoryMap = new Map();
+
+    // Create a map with category ID as the key and the category object as the value
+    categories.forEach((category) => {
+      categoryMap.set(category.id, category);
+    });
+
+    const categoryTree = [];
+
+    // Traverse each category to find its parent and add it to the parent's children array
+    categories.forEach((category) => {
+      const parentId = category.parent.id;
+
+      if (parentId !== null) {
+        const parentCategory = categoryMap.get(parentId);
+        if (parentCategory) {
+          // Create the 'children' property if it doesn't exist
+          parentCategory.children = parentCategory.children || [];
+          parentCategory.children.push(category);
+        }
+      } else {
+        // If the category has no parent, it is a root category, add it to the tree
+        categoryTree.push(category);
       }
-    }
-  
-    return tree;
+    });
+
+    return categoryTree;
   }
 }
